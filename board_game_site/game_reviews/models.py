9c6a00ca1217088f9cb6eba289f02ce0a968922b
unknown <hankpathofprogramming@gmail.com>
2020-01-02 21:32:52 +0800

    added urls model.py

--- board_game_site/game_reviews/models.py
+++ board_game_site/game_reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class GameReview(models.Model):
+    game_score = models.CharField(max_length=30)
+    comment = models.CharField(max_length=30)
+    game_photos = models.CharField(max_length=30)
+    game_objects = models.CharField(max_length=30)
+    game_rules = models.CharField(max_length=30)
+
+class Comment(models.Model):
+    id = models.CharField(max_length=30, primary_key = True)
+    commenter = models.CharField(max_length=30)
+    content = models.CharField(max_length=30)
+
